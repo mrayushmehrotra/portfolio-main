8d31626bc8ed2c3c6b1f722e1f95a58010a6ab73
ayush <ayushmehrotra099@gmail.com>
2024-08-25 23:17:16 +0530

    fixed star error

--- components/main/Footer.tsx
+++ components/main/Footer.tsx
@@ -1,20 +1,8 @@
 "use client";
 import React, { useState } from "react";
 import GitHubCalendar from "react-github-calendar";
-import { motion } from "framer-motion";
-import { slideInFromLeft } from "@/utils/motion";
 
 const Footer = () => {
-  const [showGithub, setShowGithub] = useState(false);
-  const [showVideo, setShowVideo] = useState(true);
-  const [btnText, setBtnText] = useState("Unlock");
-
-  function getGithub() {
-    setShowGithub(!showGithub);
-    setShowVideo(!showVideo);
-    setBtnText(btnText === "Unlock" ? "Lock" : "Unlock");
-  }
-
   return (
     <div>
       <center>
@@ -23,41 +11,20 @@ const Footer = () => {
             className=" flex items-center justify-center flex-col h-full w-full"
             id="about-me"
           >
-            <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-10">
+            <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-5">
               Github Contribution
             </h1>
-            {showVideo && (
-              <video
-                id="video"
-                onClick={getGithub}
-                muted
-                loop
-                autoPlay
-                className=""
-              >
-                <source src="/encryption.webm" type="video/webm" />
-              </video>
-            )}
-            <div className="z-40">
-              {showGithub && (
-                <GitHubCalendar
-                  username="mrayushmehrotra"
-                  blockSize={15}
-                  blockMargin={4}
-                  fontSize={20}
-                  hideMonthLabels
-                  hideTotalCount
-                  hideColorLegend
-                />
-              )}
+            <div className="z-50">
+              <GitHubCalendar
+                username="mrayushmehrotra"
+                blockSize={15}
+                blockMargin={4}
+                fontSize={20}
+                hideMonthLabels
+                hideTotalCount
+                hideColorLegend
+              />
             </div>
-            <motion.a
-              onClick={getGithub}
-              variants={slideInFromLeft(1)}
-              className="p-2 mt-8 z-29  border border-[#7042f861] bg-[#0300145e]  font-semibold button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px]"
-            >
-              {btnText}
-            </motion.a>
           </div>
         </div>
       </center>
